baa81a68650d7d619429ad5a659650324dac6cc6
user <user@LAPTOP-Q7AL5D2T>
2024-05-24 20:27:12 +0530

    FirstSampleJenkins for a Maven

---
+++ FirstProject/src/main/java/SampleProject/PrintClass.java
@@ -0,0 +1,16 @@
+package SampleProject;
+
+public class PrintClass {
+
+	public void print()
+	{
+		System.out.println("print");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		PrintClass p = new PrintClass();
+		p.print();
+	}
+
+}
